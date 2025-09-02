f9e0fba14b65d0d1504acc7b54446d0260ea7e90
Stefhon Jean-Claude <56743326+saderod@users.noreply.github.com>
2025-09-02 15:03:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ st.markdown(
 .stButton > button{
   font-size: 40px !important;
   font-weight: 700 !important;
-  padding: 0.9rem 1.7rem !important;
+  padding: 0.95rem 1.9rem !important;
   border-radius: 12px !important;
 }
 
@@ -496,6 +496,7 @@ with st.expander("Advanced: Reset Pipeline (danger)"):
 
 # CLOSE the shared wrapper
 st.markdown("</div>", unsafe_allow_html=True)
+
 
 
 
